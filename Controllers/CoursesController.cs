e7618a170ec92ff6b9c2c1f0e08e173b6a84b706
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2022-09-30 09:09:19 +0200

    First commit

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCourse.Controllers
+{
+    public class CoursesController : Controller
+    {
+        /* Action to display the courses list */
+        public IActionResult Index()
+        {
+            return Content("I am Index");
+        }
+
+        /* Action to display a course detail */
+        public IActionResult Detail(string courseId)
+        {
+            return Content($"I am Detail and I received the id {courseId}");
+        }
+    }
+}
