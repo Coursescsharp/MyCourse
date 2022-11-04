77ab9b0fe5621ff776dc61ca04a2351f326e12c1
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2022-11-04 15:57:18 +0100

    Organizzare la logica all'interno del model

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -1,13 +1,20 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using MyCourse.Models.Services.Application;
+using MyCourse.Models.ViewModels;
 
 namespace MyCourse.Controllers
 {
     public class CoursesController : Controller
     {
+        private CourseService courseService = new CourseService();
+
         /* Action to display the courses list */
         public IActionResult Index()
         {
-            return View();
+            List<CourseViewModel> courses = this.courseService.GetCourses();
+
+            return View(courses);
         }
 
         /* Action to display a course detail */
---
+++ Models/ViewModels/CourseViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace MyCourse.Models.ViewModels
+{
+    public class CourseViewModel
+    {
+
+    }
+}
